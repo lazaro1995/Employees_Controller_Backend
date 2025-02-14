9f400994316b57b4f603fce13da9d1a903256582
lhondares <lhondares@transportationamerica.com>
2025-02-14 14:54:15 -0500

    Update database to cloud

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('mysql://ubj7gpialfr72zil:xdmxOPKUSF97j9Xe6YPO@bekmaqogzlrycwzuiyhc-mysql.services.clever-cloud.com:3306/bekmaqogzlrycwzuiyhc'),
+            'host' => env('DB_HOST', 'bekmaqogzlrycwzuiyhc-mysql.services.clever-cloud.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'bekmaqogzlrycwzuiyhc'),
+            'username' => env('DB_USERNAME', 'ubj7gpialfr72zil'),
+            'password' => env('DB_PASSWORD', 'xdmxOPKUSF97j9Xe6YPO'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
